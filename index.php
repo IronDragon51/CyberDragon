10e3b589eb9c0ddc6599f9c070dc9741704f73bd
Cyb3rDargon <83302504+IronDragon51@users.noreply.github.com>
2023-05-07 16:18:08 +0200

    Base

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cyber Dragon - Artist and Coder</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QLMxz1M0rT+iT8jwT8b1PiK1xs3G4"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <h1>Cyber Dragon</h1>
+    <nav>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="gallery.html">Art Gallery</a></li>
+        <li><a href="projects.html">Coding Projects</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+  <section>
+      <h2>Welcome to my website!</h2>
+      <p>My name is Matthew, also known as Cyber Dragon. I am an artist and coder, and this is my online portfolio.</p>
+      <ul>
+        <li><a href="https://www.instagram.com/cyberdragon_art/" target="_blank"><img src="instagram-icon.png" alt="Instagram"></a></li>
+        <li><a href="https://twitter.com/Cyb3rDragon" target="_blank"><img src="twitter-icon.png" alt="Twitter"></a></li>
+        <li><a href="https://www.tiktok.com/@cyberdragonart" target="_blank"><img src="tiktok-icon.png" alt="TikTok"></a></li>
+      </ul>
+    </section>
+  </main>
+  <footer>
+    <p>&copy; 2023 Cyber Dragon. All rights reserved.</p>
+  </footer>
+</body>
+</html>
