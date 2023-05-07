8a1193d3b2f0ba1d4dc3b1aba5ebba1b7168a506
Cyb3rDargon <83302504+IronDragon51@users.noreply.github.com>
2023-05-07 17:06:57 +0200

    css v1

--- backend/index.php
+++ backend/index.php
@@ -1,50 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cyber Dragon - Artist and Coder</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QLMxz1M0rT+iT8jwT8b1PiK1xs3G4"
-    crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css" integrity="sha512-FGNgDh4W75N4xODyL9mp/4ovTgTfCHSAdMk3qUIZaT4g4J75xFvo4RHx+NptR9xn+Q2OFax1W8zZt+DImt/vkA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<header class="bg-dark">
-  <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-dark">
-      <a class="navbar-brand" href="#">Cyber Dragon</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+<header>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <!-- Logo and title -->
+      <a class="navbar-brand" href="#">
+        <img src="images/logo.png" alt="Logo">
+        CyberDragon
+      </a>
+
+      <!-- Navigation menu -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home</a>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="about.html">About</a>
+            <a class="nav-link" href="#art">Art</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="gallery.html">Art Gallery</a>
+            <a class="nav-link" href="#code">Code</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="projects.html">Coding Projects</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.html">Contact</a>
+            <a class="nav-link" href="#contact">Contact</a>
           </li>
         </ul>
       </div>
-    </nav>
-  </div>
+    </div>
+  </nav>
 </header>
 
 <main>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-md-6 text-center">
+      <div class="col-md-6 text-center order-2 order-md-1">
         <h1>Welcome to Cyber Dragon Art</h1>
         <p class="lead">Explore the world of digital art and programming</p>
         <ul class="list-inline">
@@ -58,12 +61,12 @@
             <a href="https://www.tiktok.com/@cyberdragonart" target="_blank"><i class="fab fa-tiktok fa-2x"></i></a>
           </li>
           <li class="list-inline-item">
-            <a href="mailto:cyberdragon.art@gmail.com" target="_blank"><i class="fas fa-envelope fa-2x"></i></a>
+            <a href="mailto:irondragon551@gmail.com" target="_blank"><i class="fas fa-envelope fa-2x"></i></a>
           </li>
         </ul>
       </div>
       <div class="col-md-6 text-center">
-        <img src="img/dragon.png" class="img-fluid" alt="Cyber Dragon Logo">
+        <img src="img/irondragon_logo.png" class="img-fluid" alt="Cyber Dragon Logo">
       </div>
     </div>
   </div>
@@ -72,6 +75,9 @@
   <footer>
     <p>&copy; 2023 Cyber Dragon. All rights reserved.</p>
   </footer>
-  
+
+  <!-- Bootstrap JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.min.js" integrity="sha512-hNUlELOeYJzQHDKaJb9m8rLJFcEDwtPmIIfFZmMPU8W6Uxyg6U5Es6UOmX6Q8W//1L37vq3qmrINB+6QatdGew==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
+
 </html>
