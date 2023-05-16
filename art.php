627e72dff3e3a524bbf408963d07b255dda703d2
Cyb3rDargon <83302504+IronDragon51@users.noreply.github.com>
2023-05-17 00:58:39 +0200

    added header.php

--- art.php
+++ art.php
@@ -1,37 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Cyber Dragon - Artist & Coder</title>
-  <link rel="shortcut icon" type="image/x-icon" href="images/favicon-16x16.png">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css" integrity="sha512-FGNgDh4W75N4xODyL9mp/4ovTgTfCHSAdMk3qUIZaT4g4J75xFvo4RHx+NptR9xn+Q2OFax1W8zZt+DImt/vkA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="animation.css">
-</head>
-
-<body>
-
-  <header>
-    <button class="navbar-toggler">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="nav-brand">
-      <img src="images/irondragon_logo.png" alt="logo" class="img-logo">
-      <a href="#">Cyber Dragon</a>
-    </div>
-    <nav>
-      <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="art.php">Art</a></li>
-        <li><a href="code.php">Code</a></li>
-        <li><a href="shop.php">Shop</a></li>
-        <li><a href="contact.php">Contact</a></li>
-      </ul>
-    </nav>
-  </header>
+<?php include 'header.php'; ?>
 
 
   <aside>
